111dfecd725fe9cb665c5310b7bbf1e5659474b2
khuongduy1484 <khuongduy1484@gmail.com>
2019-09-05 15:54:35 +0700

    multithreading

--- src/multithreading/Book.java
+++ src/multithreading/Book.java
@@ -2,29 +2,34 @@ package multithreading;
 
 import java.util.concurrent.Semaphore;
 
-public class Book   {
+public class Book {
   int count = 50;
   Semaphore semaphore = new Semaphore(50);
-public Book(){};
-public void borrowBook(){
-  synchronized (this){
+
+  public Book() {
+  }
+
+  ;
+
+  public void borrowBook() {
+    synchronized (this) {
+      try {
+        semaphore.acquire();
+      }
+      catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println("ban da thue sach thanh cong");
+    }
     try {
-      semaphore.acquire();
+      Thread.sleep(2000);
     }
     catch (InterruptedException e) {
       e.printStackTrace();
     }
-    System.out.println("ban da thue sach thanh cong");
+    synchronized (this) {
+      count--;
+    }
   }
-  try {
-    Thread.sleep(2000);
-  }
-  catch (InterruptedException e) {
-    e.printStackTrace();
-  }
-  synchronized (this){
-    count--;
-  }
-}
 
 }
--- src/multithreading/Demo.java
+++ src/multithreading/Demo.java
@@ -6,18 +6,18 @@ import java.util.concurrent.TimeUnit;
 
 public class Demo {
   public static void main(String[] args) {
-Book book = new Book();
+    Book book = new Book();
     book.borrowBook();
 
-  ExecutorService executorService = Executors.newCachedThreadPool();
-     for (int i = 0;i <=1000;i++){
-       executorService.submit(new Runnable() {
-         @Override
-         public void run() {
-           book.borrowBook();
-         }
-       });
-     }
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    for (int i = 0; i <= 1000; i++) {
+      executorService.submit(new Runnable() {
+        @Override
+        public void run() {
+          book.borrowBook();
+        }
+      });
+    }
 
     executorService.shutdown();
     try {
