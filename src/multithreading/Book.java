731dfa1b593755ccb257181c47142d684cd8e651
khuongduy1484 <khuongduy1484@gmail.com>
2019-09-05 15:51:50 +0700

    multithreading

---
+++ src/multithreading/Book.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+import java.util.concurrent.Semaphore;
+
+public class Book   {
+  int count = 50;
+  Semaphore semaphore = new Semaphore(50);
+public Book(){};
+public void borrowBook(){
+  synchronized (this){
+    try {
+      semaphore.acquire();
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("ban da thue sach thanh cong");
+  }
+  try {
+    Thread.sleep(2000);
+  }
+  catch (InterruptedException e) {
+    e.printStackTrace();
+  }
+  synchronized (this){
+    count--;
+  }
+}
+
+}
---
+++ src/multithreading/Demo.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Demo {
+  public static void main(String[] args) {
+Book book = new Book();
+    book.borrowBook();
+
+  ExecutorService executorService = Executors.newCachedThreadPool();
+     for (int i = 0;i <=1000;i++){
+       executorService.submit(new Runnable() {
+         @Override
+         public void run() {
+           book.borrowBook();
+         }
+       });
+     }
+
+    executorService.shutdown();
+    try {
+      executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
